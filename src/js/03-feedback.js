9a7b07a1e04f54675db3c2d4011dfa6bc0711629
Evgeniy Stoyan <68951809+EvgeniyStoyan@users.noreply.github.com>
2023-04-04 14:23:47 +0300

    edit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const textarea = document.querySelector('.feedback-form textarea');
-const input = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('submit', onFormSubmit);
